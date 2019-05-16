56b481698d71c761ba5650285394d9f3a7b12364
yc-lim <47224937+ycl356@users.noreply.github.com>
2019-05-16 21:27:51 +0100

    Clean up Tile.java

--- src/tiles/Tile.java
+++ src/tiles/Tile.java
@@ -1,9 +1,7 @@
 package tiles;
 
-import javafx.scene.Parent;
 import javafx.scene.layout.Pane;
 import uk.ac.cam.cl.dgk27.stateful.State;
-import uk.ac.cam.cl.dgk27.weather.Weather;
 
 public abstract class Tile extends Pane {
     //Actual graphical element, logic handled by its parent panel
